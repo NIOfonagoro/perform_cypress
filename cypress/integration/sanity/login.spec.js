ca5e8992a819b292c1ed81706d65ba8ed722f8c9
Nicholas Ofonagoro <nicholas@play-consult.co.uk>
2020-02-12 17:34:36 +0000

    Looping through users and environments

---
+++ cypress/integration/sanity/login.spec.js
@@ -0,0 +1,38 @@
+const { fullConfig } = require('../config');
+
+describe('Login Test', function () {
+
+    for (const envName in fullConfig) {
+        const demoStage = fullConfig[envName];
+
+        describe('Env name: ' + envName, function () {
+            demoStage.users.forEach((user) => {
+                it('Logs in with user: ' + user.name, function () {
+                    cy.visit(demoStage.url)
+                    cy.get('[name=email]').type(user.username)
+                    cy.get('[name=password]').type(demoStage.password)
+                    cy.get('[type=submit').click()
+
+                    if (user.isTeamLeader) {
+                        cy.url().should('include', '/employeeDashboard')
+                    } else {
+                        cy.url().should('include', '/dashboard')
+                    }
+
+
+                    // Conditional search for "what's new" modal close button
+                    cy.get('body').then(body => {
+                        if (body.find('.whats-new-button-container').length) {
+                            console.log('Found a button!!!! YAY');
+                            body.find('.whats-new-button-container').forEach(button => button.click());
+                        }
+                        else {
+                            console.log('no button found!!!')
+                        }
+                    })
+                })
+            });
+        })
+
+    }
+})
--- cypress/test.js
+++ cypress/test.js
@@ -1,8 +1,28 @@
 const config = {
-    staging: {
-        user: 'test',
-        pass: 'test'
-    },
+    staging: [
+        {
+            name: "Admin",
+            username: "nicholas+luffy@play-consult.net",
+            password: "Perform0102$"
+        },
+        {
+            name: "Team Manager",
+            username: "nicholas+zoro@play-consult.net",
+            password: "Perform0102$"
+
+        },
+        {
+            name: "Team Leader",
+            username: "nicholas+sanji@play-consult.net",
+            password: "Perform0102$",
+            isTeamLeader: true
+        },
+        {
+            name: "Employee",
+            username: "nicholas+nami@play-consult.net",
+            password: "Perform0102$"
+        }
+    ],
     prod: {
         user: 'prdtest',
         pass: 'prdtest'
